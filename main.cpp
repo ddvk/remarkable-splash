3ef79b8f7b145b51d9c7e1862b4a14313951248e
ddvk <ddvk>
2019-12-27 16:25:12 +0100

    remove unneeded code

--- main.cpp
+++ main.cpp
@@ -1,29 +1,34 @@
-#include <QCoreApplication>
 #include <QPainter>
 #include <epframebuffer.h>
-#include <QDebug>
+#include <iostream>
 
+
+void nullOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+}
 int main(int argc, char *argv[])
 {
+	qInstallMessageHandler(nullOutput);
     QString splashscreen_path = "/usr/share/remarkable/poweroff.png";
 
-    qputenv("QT_QPA_PLATFORM", "epaper");
-
-    QCoreApplication app(argc, argv);
-
-    auto arg = app.arguments();
-    if (arg.count() > 1) {
-        splashscreen_path = arg[1];
+    if (argc > 1) {
+        splashscreen_path = argv[1];
     } else {
-        qDebug() << "showing the poweroff image";
+		std::cout << "no argument supplied, showing the poweroff image\n";
     }
+    QImage img(splashscreen_path);
+	if (img.isNull()) {
+		std::cerr << "the image does not exist\n";
+		return 1;
+	}
+
     auto size = EPFrameBuffer::framebuffer()->size();
     QRect rect(0,0, size.width(), size.height());
 
     QPainter painter(EPFrameBuffer::framebuffer());
-    QImage img(splashscreen_path);
     painter.drawImage(rect, img);
-    EPFrameBuffer::sendUpdate(rect, EPFrameBuffer::Grayscale, EPFrameBuffer::FullUpdate);
+
+    EPFrameBuffer::sendUpdate(rect, EPFrameBuffer::HighQualityGrayscale, EPFrameBuffer::FullUpdate);
 
     return 0;
 }
--- remarkable-splash.pro
+++ remarkable-splash.pro
@@ -1,5 +1,7 @@
+DEFINES = QT_NO_DEBUG_OUTPUT
 CONFIG += c++11
 CONFIG += optimize_full
+CONFIG += warn_off release
 LIBS += -lqsgepaper
 
 SOURCES += \
