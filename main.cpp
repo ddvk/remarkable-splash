9c5725587a63ccd88ac4bd55b9c9adaeeadbda09
ddvk <ddvk>
2019-12-27 14:40:17 +0100

    fix whitespace

--- main.cpp
+++ main.cpp
@@ -7,18 +7,18 @@ int main(int argc, char *argv[])
 {
     QString splashscreen_path = "/usr/share/remarkable/poweroff.png";
 
-	qputenv("QT_QPA_PLATFORM", "epaper");
+    qputenv("QT_QPA_PLATFORM", "epaper");
 
     QCoreApplication app(argc, argv);
 
     auto arg = app.arguments();
-	if (arg.count() > 1) {
-		splashscreen_path = arg[1];
-	} else {
-		qDebug() << "showing the poweroff image";
-	}
-	auto size = EPFrameBuffer::framebuffer()->size();
-	QRect rect(0,0, size.width(), size.height());
+    if (arg.count() > 1) {
+        splashscreen_path = arg[1];
+    } else {
+        qDebug() << "showing the poweroff image";
+    }
+    auto size = EPFrameBuffer::framebuffer()->size();
+    QRect rect(0,0, size.width(), size.height());
 
     QPainter painter(EPFrameBuffer::framebuffer());
     QImage img(splashscreen_path);
